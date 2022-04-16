5626f8ac507743e22539cd1d176dbd0f7ee82fdc
Franz-Karl Schachinger <franz.k.schachinger@gmail.com>
2022-04-16 14:39:01 +0200

    Signed-off-by: Franz-Karl Schachinger <franz.k.schachinger@gmail.com>

---
+++ Skripts/AccessTheft.ps1
@@ -0,0 +1,25 @@
+<#
+.SYNOPSIS
+            ========================Security Implementation========================
+######                                              #     #                                                    
+#     # ######  ####  #    # #####  ####  #####     #     #   ##   #####  #####  ###### #    # # #    #  ####  
+#     # #      #      #   #    #   #    # #    #    #     #  #  #  #    # #    # #      ##   # # ##   # #    # 
+#     # #####   ####  ####     #   #    # #    #    ####### #    # #    # #    # #####  # #  # # # #  # #      
+#     # #           # #  #     #   #    # #####     #     # ###### #####  #    # #      #  # # # #  # # #  ### 
+#     # #      #    # #   #    #   #    # #         #     # #    # #   #  #    # #      #   ## # #   ## #    # 
+######  ######  ####  #    #   #    ####  #         #     # #    # #    # #####  ###### #    # # #    #  ####  
+                                                                                                               
+
+    ©Franz-Karl Schachinger, https://github.com/fkarlSchachinger/
+.DESCRIPTION
+    Sets Auto-Lock Screen Registry
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
