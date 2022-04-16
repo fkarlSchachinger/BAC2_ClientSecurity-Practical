14f46a7f926b2376af7a5bb0cfde08d9a9ccd288
Franz-Karl Schachinger <franz.k.schachinger@gmail.com>
2022-04-16 22:45:49 +0200

    completed first skript

--- Skripts/AccessTheft.ps1
+++ Skripts/AccessTheft.ps1
@@ -25,9 +25,19 @@
     ne
 #>
 function InitiateMitigations {
-    $end = "Finished"
+    Get-Module -ListAvailable GroupPolicy
+    Get-Module GroupPolicy
+    $end = "=====Finished====="
     Write-Verbose -Message "Starting Skript:"
-    New-ItemProperty -Path "HKCU\Software\Policies\Microsoft\Windows\Control Panel\Desktop" -Name "ScreenSaverTimeOut" -Value "500" -PropertyType "String"
-   
+    $GPO = New-GPO -Name "Access_Mitigations"  -Comment "Access Mitigations and Security Measures" #create new GPO
+    $GPO
+    New-GPLink -Guid $GPO.Id -Target "OU=Employees,$((Get-AdDomain).DistinguishedName)" -LinkEnabled Yes -Order 1
+    #set lock screen timer
+    Set-GPRegistryValue -Name "Access_Mitigations" -Key "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\" -ValueName "InactivityTimeoutSecs" -Value 300 -Type DWord
+    #Disable Autorun Feature
+    Set-GPRegistryValue -Name "Access_Mitigations" -Key "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\" -ValueName "NoDriveTypeAutoRun" -Value 1 -Type DWord
+    #Disable Device Installation
+    Set-GPRegistryValue -Name "Access_Mitigations" -Key "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\DeviceInstall\Restrictions" -ValueName "DenyRemovableDevices" -Value 1 -Type DWord
+    #Disabling automatic hotspot connections have to be done manually in GPO
     return $end
 }
