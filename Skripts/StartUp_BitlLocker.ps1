8ae5f9ff77b63c73b8b9951a28e6e0742fcd6bcb
Franz-Karl Schachinger <franz.k.schachinger@gmail.com>
2022-04-19 20:09:27 +0200

    BitLocker Final

--- Skripts/StartUp_BitlLocker.ps1
+++ Skripts/StartUp_BitlLocker.ps1
@@ -4,7 +4,21 @@ $test
 
 if($var.contains('Protection Status:    Protection Off')){
     manage-bde.exe -on C:
-    Write-Host 'Bitlocker: Beginning Encryption of C: Drive'
+    Write-Host 'Bitlocker: Beginning Encryption of C: Drive after reboot'
+    $title = 'Reboot for BitLocker Required:'
+    $question = 'Reboot now (Y) or later (N)?'
+    $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+    $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'))
+    $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+    $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+    if($decision -eq 0){
+        Write-Host 'Rebooting now'
+        Restart-Computer
+    }else{
+        Write-Host 'Reboot in 5 mins'
+        Start-Sleep -Seconds 300
+        Restart-Computer
+    }
 }else{
-    Write-Host 'test'
+    Write-Host 'Already Encrypted'
 }
